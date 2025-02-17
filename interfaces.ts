c0c6acf5367f94ad0f84a2c8412aed258729d01a
Muhammad Abdullah Bashir <killersaeed007@gmail.com>
2025-02-17 19:13:27 +0500

    Add files via upload

---
+++ interfaces.ts
@@ -0,0 +1,17 @@
+// app.ts
+let mes = "Hello, TypeScript!";
+console.log(mes);
+
+let num: number = 5;  // First declaration of `num`
+num = 5 + 6;
+console.log(num);
+
+mes = "again its me"
+console.log(mes)
+
+let isVALID = true
+console.log(isVALID);
+
+// Using bigint for displaying large numbers.
+let Bnumber: bigint = 5999995666666666666669999999n;
+console.log(Bnumber) 
